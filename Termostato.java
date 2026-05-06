7f5b3db0a10c551c73248d9966197f95999d73e1
Henrique <henrike.costaa@gmail.com>
2026-05-05 21:23:35 -0300

    inclusao modo eco

--- Termostato.java
+++ Termostato.java
@@ -4,4 +4,8 @@ public class Termostato {
     public Termostato(double temperaturaInicial) {
         this.temperaturaInicial = -10.0;
     }
+
+    public void ativarEconomia(){
+        temperaturaInicial = 18;
+    }
 }
